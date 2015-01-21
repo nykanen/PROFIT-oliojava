5b2df744ba50b4200b9578f8644ece13dceb000a
nykanen <nykanen.lauri@gmail.com>
2015-01-21 10:54:54 +0200

    Update Kokonaisluvut.java
    Kommenttien lisäys.

--- src/nykanen/Kokonaisluvut.java
+++ src/nykanen/Kokonaisluvut.java
@@ -1,18 +1,36 @@
 package nykanen;
 import java.lang.Math;
 
+/**
+ * This class prints out integers from 1 to 1000 that have an integer root.
+ * 
+ * @author Lauri Nykänen
+ *
+ */
 public class Kokonaisluvut {
     
-    public static boolean isInteger(double number) {	
-        double smaller = Math.floor(number);
-        double bigger = Math.ceil(number);
+    /**
+     * This method validates whether a number is an integer.
+     * 
+     * @param square of the integer
+     * @return boolean
+     */
+    public static boolean isInteger(double square) {    
+        double smaller = Math.floor(square);
+        double bigger = Math.ceil(square);
         final double EPSILON = 0.000001;
-        if (number - smaller < EPSILON || bigger - number < EPSILON) {
+        if (square - smaller < EPSILON || bigger - square < EPSILON) {
             return true;
         }
         return false;
     }
 
+    /**
+     * Main method that iterates through integers from 1 to 1000
+     * and prints out the integers that have an integer square root.
+     * 
+     * @param args not used
+     */
     public static void main(String[] args) {
         for (int i=1; i < 1001; i++) {
             double squareRoot = Math.sqrt(i);
